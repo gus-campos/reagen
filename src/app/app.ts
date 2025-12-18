33a4817d22dfc0277de0b9c3eb9f3c13f994db76
Gustavo Campos <gus.campos042@gmail.com>
2025-12-17 21:53:04 -0300

    complete base config

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('reagen');
+  readonly count = signal(0);
+
+  inc() {
+    this.count.update((c) => c + 1);
+  }
 }
