b338f055587194aa15e83494616c922088566891
Gustavo Campos <gus.campos042@gmail.com>
2026-01-20 20:55:20 -0300

    adiciona console para debugar deploy

--- src/core/config/firebase.ts
+++ src/core/config/firebase.ts
@@ -4,6 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { firebaseConfigProd } from '@/core/config/firebase.prod';
 import { firebaseConfigTest } from '@/core/config/firebase.tests';
 
+console.log({
+  mode: process.env.NEXT_PUBLIC_MODE,
+  prod: process.env.NEXT_PUBLIC_FIREBASE_PROD_API_KEY,
+  test: process.env.NEXT_PUBLIC_FIREBASE_TEST_API_KEY,
+});
+
 const env = process.env.NEXT_PUBLIC_MODE;
 
 export const firebaseConfig = env === 'test' ? firebaseConfigTest : firebaseConfigProd;
