045caa55831bf1a03b2822a055e62293fb6c021a
Gustavo Campos <gus.campos042@gmail.com>
2026-02-03 21:58:24 -0300

    melhora filtros

--- src/features/stock-filter/components/filter-options.view.tsx
+++ src/features/stock-filter/components/filter-options.view.tsx
@@ -3,11 +3,14 @@ import { DatePickerInput } from '@mantine/dates';
 import { useFilterOptions } from '@/features/stock-filter/components/filter-options.viewmodel';
 import { StockFilter } from '@/features/stock-filter/stock-filter.type';
 import { portugueseSearchFilter } from '@/shared/utils/portuguese-search-filter';
+import classes from './filter-options.module.css';
 
 function TouchedBadge(props: { text: string; active: boolean }) {
   return (
     <Group gap="xs">
-      <Text fw="bold">{props.text}</Text>
+      <Text fw="bold" size="14px">
+        {props.text}
+      </Text>
       {props.active && <Badge circle size="10px" color="cyan" />}
     </Group>
   );
@@ -45,36 +48,40 @@ export function FilterOptions(props: FilterOptionsProps) {
       </Title>
 
       <form>
-        <Accordion variant="default" chevronPosition="left">
+        <Accordion variant="contained" maw={400} classNames={classes}>
           {/* Entrada */}
           <Accordion.Item value="in-date">
             <Accordion.Control>
               <Group>
-                <TouchedBadge active={isInDateFilterActive} text="Por entrada" />
+                <TouchedBadge active={isInDateFilterActive} text="Entrada" />
               </Group>
             </Accordion.Control>
             <Accordion.Panel>
-              <Grid mt="md">
-                <Grid.Col span={{ base: 6 }}>
-                  <DatePickerInput
-                    clearable
-                    valueFormat="DD/MM/YYYY"
-                    label="A partir de"
-                    placeholder="Selecione"
-                    {...form.getInputProps('minInDate')}
-                  />
-                </Grid.Col>
+              {/* <InputWrapper label="Filtre por"> */}
+              <Paper p="md" withBorder>
+                <Grid>
+                  <Grid.Col span={{ base: 6 }}>
+                    <DatePickerInput
+                      clearable
+                      valueFormat="DD/MM/YYYY"
+                      label="A partir de"
+                      placeholder="Selecione"
+                      {...form.getInputProps('minInDate')}
+                    />
+                  </Grid.Col>
 
-                <Grid.Col span={{ base: 6 }}>
-                  <DatePickerInput
-                    clearable
-                    valueFormat="DD/MM/YYYY"
-                    label="Até"
-                    placeholder="Selecione"
-                    {...form.getInputProps('maxInDate')}
-                  />
-                </Grid.Col>
-              </Grid>
+                  <Grid.Col span={{ base: 6 }}>
+                    <DatePickerInput
+                      clearable
+                      valueFormat="DD/MM/YYYY"
+                      label="Até"
+                      placeholder="Selecione"
+                      {...form.getInputProps('maxInDate')}
+                    />
+                  </Grid.Col>
+                </Grid>
+              </Paper>
+              {/* </InputWrapper> */}
             </Accordion.Panel>
           </Accordion.Item>
 
@@ -82,38 +89,40 @@ export function FilterOptions(props: FilterOptionsProps) {
           <Accordion.Item value="out-date">
             <Accordion.Control>
               <Group>
-                <TouchedBadge active={isOutDateFilterActive} text="Por saída" />
+                <TouchedBadge active={isOutDateFilterActive} text="Saída" />
               </Group>
             </Accordion.Control>
             <Accordion.Panel>
-              {/* Range de data */}
-              {/* FIXME: Apagar o outro campo quando mudar */}
-              <Radio.Group label="Mostrar" {...form.getInputProps('outStatus')}>
-                <Radio label="Todos" value="all" />
-                <Radio label="Apenas com saída" value="is-out" />
-                <Radio label="Apenas sem saída" value="not-out" />
-              </Radio.Group>
-              <Grid mt="md">
-                <Grid.Col span={{ base: 6 }}>
-                  <DatePickerInput
-                    clearable
-                    valueFormat="DD/MM/YYYY"
-                    label="A partir de"
-                    placeholder="Selecione"
-                    {...form.getInputProps('minOutDate')}
-                  />
-                </Grid.Col>
+              <Paper p="md" withBorder>
+                {/* Range de data */}
+                {/* FIXME: Apagar o outro campo quando mudar */}
+                <Radio.Group label="Mostrar" {...form.getInputProps('outStatus')}>
+                  <Radio label="Todos" value="all" />
+                  <Radio label="Com saída" value="is-out" />
+                  <Radio label="Sem saída" value="not-out" />
+                </Radio.Group>
+                <Grid mt="md">
+                  <Grid.Col span={{ base: 6 }}>
+                    <DatePickerInput
+                      clearable
+                      valueFormat="DD/MM/YYYY"
+                      label="A partir de"
+                      placeholder="Selecione"
+                      {...form.getInputProps('minOutDate')}
+                    />
+                  </Grid.Col>
 
-                <Grid.Col span={{ base: 6 }}>
-                  <DatePickerInput
-                    clearable
-                    valueFormat="DD/MM/YYYY"
-                    label="Até"
-                    placeholder="Selecione"
-                    {...form.getInputProps('maxOutDate')}
-                  />
-                </Grid.Col>
-              </Grid>
+                  <Grid.Col span={{ base: 6 }}>
+                    <DatePickerInput
+                      clearable
+                      valueFormat="DD/MM/YYYY"
+                      label="Até"
+                      placeholder="Selecione"
+                      {...form.getInputProps('maxOutDate')}
+                    />
+                  </Grid.Col>
+                </Grid>
+              </Paper>
             </Accordion.Panel>
           </Accordion.Item>
 
@@ -121,123 +130,132 @@ export function FilterOptions(props: FilterOptionsProps) {
           <Accordion.Item value="expire-date">
             <Accordion.Control>
               <Group>
-                <TouchedBadge active={isExpireDateFilterActive} text="Por vencimento" />
+                <TouchedBadge active={isExpireDateFilterActive} text="Vencimento" />
               </Group>
             </Accordion.Control>
             <Accordion.Panel>
-              {/* Range de data */}
-              <Radio.Group label="Mostrar" {...form.getInputProps('expired')}>
-                <Radio label="Todos" value="all" />
-                <Radio label="Apenas vencidos" value="expired" />
-                <Radio label="Apenas não vencidos" value="not-expired" />
-              </Radio.Group>
-              <Grid mt="md">
-                <Grid.Col span={{ base: 6 }}>
-                  <DatePickerInput
-                    clearable
-                    valueFormat="DD/MM/YYYY"
-                    label="A partir de"
-                    placeholder="Selecione"
-                    {...form.getInputProps('minExpire')}
-                  />
-                </Grid.Col>
+              <Paper p="md" withBorder>
+                {/* Range de data */}
+                <Radio.Group label="Mostrar" {...form.getInputProps('expired')}>
+                  <Radio label="Todos" value="all" />
+                  <Radio label="vencidos" value="expired" />
+                  <Radio label="não vencidos" value="not-expired" />
+                </Radio.Group>
+                <Grid mt="md">
+                  <Grid.Col span={{ base: 6 }}>
+                    <DatePickerInput
+                      clearable
+                      valueFormat="DD/MM/YYYY"
+                      label="A partir de"
+                      placeholder="Selecione"
+                      {...form.getInputProps('minExpire')}
+                    />
+                  </Grid.Col>
 
-                <Grid.Col span={{ base: 6 }}>
-                  <DatePickerInput
-                    clearable
-                    valueFormat="DD/MM/YYYY"
-                    label="Até"
-                    placeholder="Selecione"
-                    {...form.getInputProps('maxExpire')}
-                  />
-                </Grid.Col>
-              </Grid>
+                  <Grid.Col span={{ base: 6 }}>
+                    <DatePickerInput
+                      clearable
+                      valueFormat="DD/MM/YYYY"
+                      label="Até"
+                      placeholder="Selecione"
+                      {...form.getInputProps('maxExpire')}
+                    />
+                  </Grid.Col>
+                </Grid>
+              </Paper>
             </Accordion.Panel>
           </Accordion.Item>
 
           <Accordion.Item value="controlled">
             <Accordion.Control>
-              <TouchedBadge active={isControlledFilterActive} text="Por controle" />
+              <TouchedBadge active={isControlledFilterActive} text="Controle" />
             </Accordion.Control>
             <Accordion.Panel>
-              <Radio.Group label="Mostrar" {...form.getInputProps('controlled')}>
-                <Radio label="Todos" value="all" />
-                <Radio label="Apenas controlados" value="controlled" />
-                <Radio label="Apenas não controlados" value="not-controlled" />
-              </Radio.Group>
+              <Paper p="md" withBorder>
+                <Radio.Group label="Mostrar" {...form.getInputProps('controlled')}>
+                  <Radio label="Todos" value="all" />
+                  <Radio label="Controlados" value="controlled" />
+                  <Radio label="Não controlados" value="not-controlled" />
+                </Radio.Group>
 
-              <Select
-                searchable
-                clearable
-                mt="md"
-                label="Orgão de controle"
-                placeholder="Escolha o orgão de controle"
-                value={form.values.controlAgencyId}
-                data={controlAgencyOptions}
-                onChange={handleControlAgencyChange}
-                filter={portugueseSearchFilter}
-              />
+                <Select
+                  searchable
+                  clearable
+                  mt="md"
+                  label="Orgão de controle"
+                  placeholder="Escolha o orgão de controle"
+                  value={form.values.controlAgencyId}
+                  data={controlAgencyOptions}
+                  onChange={handleControlAgencyChange}
+                  filter={portugueseSearchFilter}
+                />
+              </Paper>
             </Accordion.Panel>
           </Accordion.Item>
+
           <Accordion.Item value="byFundingSource">
             <Accordion.Control>
-              <TouchedBadge active={isFundingSourceFilterActive} text="Por adquirente" />
+              <TouchedBadge active={isFundingSourceFilterActive} text="Adquirente" />
             </Accordion.Control>
             <Accordion.Panel>
-              <Radio.Group label="Mostrar" {...form.getInputProps('fundingScope')}>
-                <Radio label="Todos" value="all" />
-                <Radio label="Apenas interno" value="internal" />
-                <Radio label="Apenas externo" value="external" />
-              </Radio.Group>
-              <Select
-                searchable
-                clearable
-                mt="md"
-                label="Adquirente"
-                placeholder="Escolha o adquirente"
-                value={form.values.fundingSourceId}
-                data={fundingSourceOptions}
-                onChange={handleFundingSourceChange}
-                filter={portugueseSearchFilter}
-              />
+              <Paper p="md" withBorder>
+                <Radio.Group label="Mostrar" {...form.getInputProps('fundingScope')}>
+                  <Radio label="Todos" value="all" />
+                  <Radio label="Interno" value="internal" />
+                  <Radio label="Externo" value="external" />
+                </Radio.Group>
+                <Select
+                  searchable
+                  clearable
+                  mt="md"
+                  label="Adquirente"
+                  placeholder="Escolha o adquirente"
+                  value={form.values.fundingSourceId}
+                  data={fundingSourceOptions}
+                  onChange={handleFundingSourceChange}
+                  filter={portugueseSearchFilter}
+                />
+              </Paper>
             </Accordion.Panel>
           </Accordion.Item>
 
           <Accordion.Item value="byLaboratory">
             <Accordion.Control>
-              <TouchedBadge active={isLaboratoryFilterActive} text="Por laboratório" />
+              <TouchedBadge active={isLaboratoryFilterActive} text="Laboratório" />
             </Accordion.Control>
             <Accordion.Panel>
-              <Select
-                searchable
-                clearable
-                mt="md"
-                label="Laboratório"
-                placeholder="Escolha o laboratório"
-                value={form.values.laboratoryId}
-                data={laboratoryOptions}
-                onChange={handleLaboratoryChange}
-                filter={portugueseSearchFilter}
-              />
+              <Paper p="md" withBorder>
+                <Select
+                  searchable
+                  clearable
+                  label="Laboratório"
+                  placeholder="Escolha o laboratório"
+                  value={form.values.laboratoryId}
+                  data={laboratoryOptions}
+                  onChange={handleLaboratoryChange}
+                  filter={portugueseSearchFilter}
+                />
+              </Paper>
             </Accordion.Panel>
           </Accordion.Item>
 
           <Accordion.Item value="bySupplier">
             <Accordion.Control>
-              <TouchedBadge active={isSupplierFilterActive} text="Por fornecedor" />
+              <TouchedBadge active={isSupplierFilterActive} text="Fornecedor" />
             </Accordion.Control>
             <Accordion.Panel>
-              <Select
-                searchable
-                clearable
-                mt="md"
-                label="Fornecedor"
-                placeholder="Escolha o fornecedor"
-                value={form.values.supplierId}
-                data={supplierOptions}
-                onChange={handleSupplierChange}
-                filter={portugueseSearchFilter}
-              />
+              <Paper p="md" withBorder>
+                <Select
+                  searchable
+                  clearable
+                  label="Fornecedor"
+                  placeholder="Escolha o fornecedor"
+                  value={form.values.supplierId}
+                  data={supplierOptions}
+                  onChange={handleSupplierChange}
+                  filter={portugueseSearchFilter}
+                />
+              </Paper>
             </Accordion.Panel>
           </Accordion.Item>
         </Accordion>
